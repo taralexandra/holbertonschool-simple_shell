a7c827432ae85415079e231fcb11b103a5d86f71
taralexandra <taralexandra@gmail.com>
2024-08-19 11:15:28 +0200

    I am adding the file called main.c for the simple shell project

---
+++ main.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+  * define PROMPT - here, we define the constant prompt that will
+  * be displayed when we execute the program
+  * and before to enter a command.
+  */
+
+#define PROMPT "#cisfun$ "
+
+/**
+  * define EXIT_COMMAND - this command "exit" allow to quit the program.
+  */
+
+#define EXIT_COMMAND "exit"
+
+/**
+  * read_command - read the command entered by user.
+  * @line: ptr to the buffer where we stock the command.
+  * @len: ptr to the size of the buffer.
+  * Return: the number of char which are read, or -1
+  * in case of issue.
+  */
+ssize_t read_command(char **line, size_t *len)
+{
+	ssize_t nread;
+
+	printf(PROMPT);
+
+	nread = getline(line, len, stdin);
+
+	if (nread == -1)
+	{
+		if (feof(stdin))
+		{
+			printf("\n");
+			return (-1);
+		}
+		perror("getline");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*line)[nread - 1] == '\n')
+	{
+		(*line)[nread - 1] = '\0';
+	}
+
+	return (nread);
+}
+
+/**
+  * exec_command - this func executes the command entered.
+  * @line: the command to execute.
+  */
+void exec_command(char *line)
+{
+	char *args[2];
+
+	args[0] = line;
+	args[1] = NULL;
+
+	if (fork() == 0)
+	{
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror(line);
+		}
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
+
+
+
+/**
+  * main - this is the main function for simple shell.
+  * The prompt will be displayed "#cisfun$"
+  * So the user can enter a command like /bin/ls.
+  * And the command entered will be executed.
+  * Return: 0 in case of success.
+  */
+
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+
+	while (1)
+	{
+
+		nread = read_command(&line, &len);
+
+		if (nread == -1)
+		{
+			break;
+		}
+		if (strcmp(line, EXIT_COMMAND) == 0)
+		{
+			break;
+		}
+
+		exec_command(line);
+	}
+
+
+	free(line);
+	return (0);
+}
