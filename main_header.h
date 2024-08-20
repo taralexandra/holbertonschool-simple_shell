aa0890b3ee1835341707f67b6f351a574f6c9aa0
taralexandra <taralexandra@gmail.com>
2024-08-20 11:37:39 +0200

    testing version of main.c with header file

---
+++ main_header.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_HEADER_H
+#define MAIN_HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+/**
+  * define PROMPT - here, we define the constant prompt that will
+  * be displayed when we execute the program
+  * and before to enter a command.
+  */
+
+#define PROMPT "#cisfun$ "
+
+/**
+  * define EXIT_COMMAND - this command "exit" allow to quit the program.
+  */
+
+#define EXIT_COMMAND "exit"
+
+
+ssize_t read_command(char **line, size_t *len);
+void exec_command(char *line);
+
+
+#endif
