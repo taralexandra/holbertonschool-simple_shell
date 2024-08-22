a70d9f7f38a607f4e250efbd4057ff69e8bebb39
Saki <saki@LeHote.myguest.virtualboxorg>
2024-08-22 10:41:38 +0200

    New version of simple shell, fixed memory leaks and exit message

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+int main(int argc, char *argv[], char *env[]);
+void execute_command(char *name, char *command, char **line, char **env);
+
+#endif
